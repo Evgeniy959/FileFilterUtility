19f88dbc4fc6dee3638720954929b027146b5fe3
Evgeniy959 <77540303+Evgeniy959@users.noreply.github.com>
2025-02-14 05:40:31 +0700

    del file

--- FileFilterUtility/src/main/java/org/example/options/TypeFile.java
+++
@@ -1,7 +0,0 @@
-package org.example.datamanager;
-
-public class TypeFile {
-    public static String INTEGER = "integers";
-    public static String FLOAT = "floats";
-    public static String STRING = "strings";
-}
